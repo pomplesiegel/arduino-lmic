4cdd636a2989ea42d6acb1cc48abec038afb50b7
Michael Siegel <pomplesiegel@gmail.com>
2025-02-14 16:02:44 -0500

    Selecting region by ifdef

--- project_config/lmic_project_config.h
+++ project_config/lmic_project_config.h
@@ -1,8 +1,17 @@
 // project-specific definitions
-//#define CFG_eu868 1
-#define CFG_us915 1
+#ifdef REGION_US915
+    #define CFG_us915 1
+#endif
+
+#ifdef REGION_AS923
+    #define CFG_as923 1
+#endif
+
+#ifdef REGION_EU868
+    #define CFG_eu868 1
+#endif
+
 //#define CFG_au915 1
-//#define CFG_as923 1
 // #define LMIC_COUNTRY_CODE LMIC_COUNTRY_CODE_JP      /* for as923-JP; also define CFG_as923 */
 //#define CFG_kr920 1
 //#define CFG_in866 1
@@ -11,3 +20,5 @@
 //#define CFG_sx1262_radio 1
 //#define ARDUINO_heltec_wifi_lora_32_V3
 //#define LMIC_USE_INTERRUPTS
+
+#define LMIC_ENABLE_arbitrary_clock_error 1
